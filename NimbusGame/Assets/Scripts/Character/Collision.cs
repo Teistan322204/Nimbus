894c45930d55df31dc8d9684f00c52557c41b591
AlexPinekone <a335770@alumnos.uaslp.mx>
2025-01-30 18:05:25 -0600

    feat/ Added movement and jump
    -The jump is improved because of a Youtube video B)

---
+++ NimbusGame/Assets/Scripts/Character/Collision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+	//Variable donde se le colocan los layers con los que se colisiona
+	[Header("Layers")]
+	public LayerMask groundLayer;
+	
+	//Booleano para saber si está en el suelo(se va a actualizar cada frame)
+	[Space]
+	public bool onGround;
+	
+	//Burbuja de colisión alrededor del personaje
+	[Space]
+	[Header("Collision")]
+	public float collisionRadius = 0.25f;
+	//Esto va a cambiar depende del sprite
+	public Vector2 bottomOffset;
+
+	void Start()
+	{
+        
+	}
+
+
+	// Revisa si está tocando los layer elegidos cada frame, según la burbuja
+    void Update()
+    {
+	    onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+    }
+}
---
+++ NimbusGame/Assets/Scripts/Character/Movement.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	//Hay que obtener cada frame si está tocando el suelo
+	private Collision coll;
+	//Modificar las fisicas y el movimiento
+	private Rigidbody2D rb;
+	
+	//Lo que dice... Stats
+	[Space]
+	[Header("Stats")]
+	public float speed = 10;
+	public float jumpForce = 70;
+	
+	//Variables para verificar la situación del jugador.
+	[Space]
+	[Header("Booleans")]
+	public bool canMove = true;
+	
+	
+	void Start()
+	{
+		//Obtiene el rigidBody del jugador
+		rb = GetComponent<Rigidbody2D>();
+		//Obtiene quien actualiza las colisiones
+		coll = GetComponent<Collision>();
+	}
+	
+	void Update()
+	{
+		//Obtiene la flecha que está presionando para moverse
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+		//Valores de movimiento dentro del Vector2
+		Vector2 dir = new Vector2(x, y);
+		
+		//Llamada a la función que mueve al personaje lo indicado
+		Walk(dir);
+		
+		if (Input.GetButtonDown("Jump"))
+		{
+
+			if (coll.onGround)
+				Jump(Vector2.up);
+		}
+	}
+	
+	//Función para producir el movimiento
+	private void Walk(Vector2 dir)
+	{
+		//Si algo lo detiene, amonos de aquí
+		if (!canMove)
+			return;
+		//Solo es el movimiento horizonta, el salto va aparte
+		rb.velocity = (new Vector2(dir.x * speed, rb.velocity.y));
+	}
+	
+	//Pide la dirección para cuando haya salto en las paredes
+	private void Jump(Vector2 dir)
+	{
+		rb.velocity = new Vector2(rb.velocity.x, 0);
+		rb.velocity += dir * jumpForce;
+		
+	}
+}
