b793a62fae4b2bba4cb580379dc882fc3b61c645
AlexPinekone <a335770@alumnos.uaslp.mx>
2025-01-31 17:25:08 -0600

    /feat wall and floor detection
    -uses a collisionRadius and an offset to detect walls and floor

--- NimbusGame/Assets/Scripts/Character/Collision.cs
+++ NimbusGame/Assets/Scripts/Character/Collision.cs
@@ -8,16 +8,20 @@ public class Collision : MonoBehaviour
 	[Header("Layers")]
 	public LayerMask groundLayer;
 	
-	//Booleano para saber si está en el suelo(se va a actualizar cada frame)
+	//Booleano para saber si está en el suelo o en una pared(se va a actualizar cada frame)
 	[Space]
 	public bool onGround;
+	public bool onWall;
+	public bool onRightWall;
+	public bool onLeftWall;
+	public int wallSide;
 	
 	//Burbuja de colisión alrededor del personaje
 	[Space]
 	[Header("Collision")]
 	public float collisionRadius = 0.25f;
 	//Esto va a cambiar depende del sprite
-	public Vector2 bottomOffset;
+	public Vector2 bottomOffset,rightOffset, leftOffset;
 
 	void Start()
 	{
@@ -27,7 +31,17 @@ public class Collision : MonoBehaviour
 
 	// Revisa si está tocando los layer elegidos cada frame, según la burbuja
     void Update()
-    {
-	    onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+	{
+		//Revisa el suelo
+		onGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, collisionRadius, groundLayer);
+		//Revisa si está pegado en la pared derecha
+		onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
+		//Revisa si está pegado a la pared izquierda
+		onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+	    
+		//Revisa si está en alguna pared
+		onWall = onRightWall || onLeftWall;
+		//Revisa CUAL pared
+	    wallSide = onRightWall ? -1 : 1;   
     }
 }
